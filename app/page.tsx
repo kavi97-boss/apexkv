71a6b12f574fb01875ff4a28ea5850e7d14beff6
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-08-30 14:10:47 +0930

    made some minor changes in page for test deployment flow

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ function Home() {
 		<div className="w-full h-screen flex flex-col justify-center items-center">
 			<Image src={'/logo.png'} alt="ApexKV Logo" width={150} height={150} />
 			<Image src={'/logo-text.png'} alt="ApexKV Logo Text" className="w-3/4 sm:w-4/5" style={{ width: '70vw' }} />
-			<h1 className="text-5xl font-bold text-white">Site on Under Construction</h1>
+			<h1 className="text-5xl font-bold text-white text-center">Site on Under Construction</h1>
 		</div>
 	);
 }
