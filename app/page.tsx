ee7c23ef46b4c1f266303c390df0ef6592c027eb
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-08-30 14:08:26 +0930

    made some minor changes in page for test deployment flow

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 function Home() {
 	return (
 		<div className="w-full h-screen flex flex-col justify-center items-center">
-			<Image src={'/logo.png'} alt="ApexKV Logo" width={100} height={100} />
-			<Image src={'/logo-text.png'} alt="ApexKV Logo Text" style={{ width: '90vw' }} />
-			<h1 className="text-6xl font-bold text-white">Page on Under Construction</h1>
+			<Image src={'/logo.png'} alt="ApexKV Logo" width={150} height={150} />
+			<Image src={'/logo-text.png'} alt="ApexKV Logo Text" className="w-3/4 sm:w-4/5" style={{ width: '70vw' }} />
+			<h1 className="text-5xl font-bold text-white">Site on Under Construction</h1>
 		</div>
 	);
 }
