bfa421a7169e50abed54898aaaf32f73edf44abc
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-08-30 13:27:46 +0930

    logos moved to public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
-import logo from '@/app/logo.png';
-import logoTxt from '@/app/logo-text.png';
 
 function Home() {
 	return (
 		<div className="w-full h-screen flex flex-col justify-center items-center">
-			<Image src={logo} alt="ApexKV Logo" width={100} height={100} />
-			<Image src={logoTxt} alt="ApexKV Logo Text" style={{ width: '90vw' }} />
+			<Image src={'/logo.png'} alt="ApexKV Logo" width={100} height={100} />
+			<Image src={'/logo-text.png'} alt="ApexKV Logo Text" style={{ width: '90vw' }} />
 			<h1 className="text-6xl font-bold text-white">Page on Under Construction</h1>
 		</div>
 	);
