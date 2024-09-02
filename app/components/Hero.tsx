e0bc743fdd5811ce4f84b6759640b4f8a6604926
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-03 01:38:32 +0930

    working on about section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ import MagicButton from './ui/MagicButton';
 
 const Hero = () => {
 	return (
-		<div className="pb-20 pt-36">
+		<div className="pb-20 pt-36 relative">
 			<div>
 				<Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
 				<Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="magenta" />
@@ -26,7 +26,7 @@ const Hero = () => {
 
 			<div className="flex justify-center relative my-20 z-10">
 				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-					<TextGenerateEffect duration={1.5} words="Transforming Complex Ideas into Interactive Digital User Experiences" />
+					<TextGenerateEffect duration={1.5} words="Transforming Complex Ideas into Interactive Digital Experiences" />
 					<p className="text-white text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
 						Hi! I&apos;m Kavindu, a Full-Stack Developer based in Adelaide, Australia.
 					</p>
